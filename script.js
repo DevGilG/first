30afa3b1f0ea98db8bfa79897e9e267d0acc3f2b
Gil <gilson.guedin@hotmail.com>
2023-02-27 15:25:43 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  }
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Gilson Guedin sorrindo, de barba, camisa social, blazer e fundo criativo branco no exterior e formas circulares roxas no interior"
+    )
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Gilson Guedin sorrindo, de barba, camisa social, blazer e fundo criativo roxo escuro gradiente com laranja"
+    )
+  }
+}
+
+// pode ser substituido por       //
+// html.classlist.toggle('light') //
